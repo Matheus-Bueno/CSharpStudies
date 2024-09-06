288cffaa0a7fa14ff3b2a32fa6da5c0c56579b82
Matheus-Bueno <38436505+Matheus-Bueno@users.noreply.github.com>
2024-09-06 14:07:04 -0300

    .

--- Exercicios/MicrosoftLearn/UsandoPackages.cs
+++ Exercicios/MicrosoftLearn/UsandoPackages.cs
@@ -3,27 +3,19 @@ using Humanizer;
 
 internal class UsandoPackages
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Quantities:");
-        HumanizeQuantities();
-
-        Console.WriteLine("\nDate/Time Manipulation:");
-        HumanizeDates();
-
-        static void HumanizeQuantities()
+       public static void HumanizeQuantities()
         {
             Console.WriteLine("case".ToQuantity(0));
             Console.WriteLine("case".ToQuantity(1));
             Console.WriteLine("case".ToQuantity(5));
         }
 
-        static void HumanizeDates()
+        public static void HumanizeDates()
         {
             Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
             Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
             Console.WriteLine(TimeSpan.FromDays(1).Humanize());
             Console.WriteLine(TimeSpan.FromDays(16).Humanize());
         }
-    }
+    
 }
